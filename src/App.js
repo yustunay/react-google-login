2eb66f65e8a9adde8ed3ec85315c577b894964db
yahya.ustunay <yahya.ustunay@azoomee.com>
2022-11-03 14:28:01 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import GoogleLogin from "react-google-login";
+import { loadGapiInsideDOM } from "gapi-script";
 
 function App() {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+
+  const handleFailure = (result) => {
+    //alert(result);
+    console.log(result);
+  };
+  const handleLogin = async (googleData) => {
+    const res = await fetch("http://localhost:8080/api/google/token/verify", {
+      method: "POST",
+      body: JSON.stringify({ token: googleData.tokenId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem("loginData", JSON.stringify(data));
+    console.log(data);
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await loadGapiInsideDOM();
+    })();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Google Login App</h1>
+        <div>
+          {loginData ? (
+            <div>
+              <h3>You logged in as {loginData.email}</h3>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              buttonText="Log in with Google"
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              cookiePolicy={"single_host_origin"}
+            ></GoogleLogin>
+          )}
+        </div>
       </header>
     </div>
   );
